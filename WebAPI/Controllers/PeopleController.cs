9f36240e6da28d399891201bdcb5ecfe0dfdb4bc
mervesiseci@gmail.com <mervesiseci@gmail.com>
2021-09-12 18:59:15 +0300

    .

--- WebAPI/Controllers/PeopleController.cs
+++ WebAPI/Controllers/PeopleController.cs
@@ -26,7 +26,7 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
-            ////Thread.Sleep(2000);
+            Thread.Sleep(2000);
             var result = _personService.GetAll();
             if (result.Success)
             {
